e6b3dbb1416eb4048134623cee0d8a262b7d2a2a
Venom-0ff <90479045+Venom-0ff@users.noreply.github.com>
2022-08-03 00:29:08 -0400

    The simulation now runs. GUI & related stuff left.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Random;
 
 /**
 * The {@code Person} class represents a person as a point in a two-dimensional space.
@@ -8,7 +9,9 @@ public class Person {
     // class members
     private boolean isAlive;
     private boolean isInfected;
-    private int immunityStatus;
+    private int immunityStatus;         //  0,   1,    2,    3,    4,    5. NOTE: '0' is only used to create the first infected person.
+    private final double[] INFECT_CHANCE = {0, 0.8,  0.6,  0.3,  0.1,  0.4};
+    private final double[] DEATH_CHANCE  = {0, 0.1, 0.07, 0.03, 0.01, 0.03};
 
     private Color colour;
 
@@ -35,9 +38,57 @@ public class Person {
         this.cycleCount = 0;
     }
 
-    // TODO:
-    // Write necessary constructors.
-    //   Randomly assign values from -5 to 5 to xInc and yInc: (int) ((Math.random() * (max - min)) + min).
+    /**
+     * Creates an instance of the {@code Person} class object.
+     * @param immunityStatus
+     * @param width width of the {@code JPanel}.
+     * @param height height of the {@code JPanel}.
+     */
+    public Person(int immunityStatus, int width, int height) {
+        this.isAlive = true;
+        this.isInfected = false;
+
+        this.immunityStatus = immunityStatus;
+        switch (this.immunityStatus) {
+            case 0:
+                this.colour = Color.RED;
+                this.isInfected = true;
+                this.immunityStatus = 1;
+                break;
+            case 1:
+                this.colour = Color.BLUE;
+                break;
+            case 2:
+                this.colour = Color.CYAN;
+                break;
+            case 3:
+                this.colour = Color.YELLOW;
+                break;
+            case 4:
+                this.colour = Color.MAGENTA;
+                break;
+        }
+
+        // generate random x and y spawn coordinates
+        int spawnX, spawnY;
+        do {
+            spawnX = (int)(Math.random() * width);
+        } while (spawnX < 0 || spawnX > width - DIAMETER);
+        this.xCoord = spawnX;
+
+        do {
+            spawnY = (int)(Math.random() * height);
+        } while (spawnY < 0 || spawnY > height - DIAMETER);
+        this.yCoord = spawnY;
+
+        // generate random xInc and yInc values
+        do {
+            this.xInc = (int) (Math.random() * 10 - 5);
+            this.yInc = (int) (Math.random() * 10 - 5);
+        } while (this.xInc == 0 && this.yInc == 0);
+
+        this.cycleCount = 0;
+    }
     
     /**
      * Moves the points inside the {@code JPanel}.
@@ -46,27 +97,18 @@ public class Person {
      * @param height height of the {@code JPanel}.
      */
     public static void move(Person person, int width, int height) {
-        if (person.getXCoord() >= width - DIAMETER)
-		{
-			// at right edge, so negate xInc
-			person.setXInc(-person.getXInc());
-		}
-		if (person.getXCoord() <= 0)
-		{
-			// at left edge, so negate xInc
-			person.setXInc(-person.getXInc());
-		}
-		if (person.getYCoord() >= height - DIAMETER)
-		{
-			// at bottom edge, so negate yInc
-            person.setYInc(-person.getYInc());
-		}
-		if (person.getYCoord() <= 0)
-		{
-			// at top edge, so negate yInc
-			person.setYInc(-person.getYInc());
-		}
+        // at left edge, so negate xInc
+		if (person.getXCoord() <= 0) { person.setXInc(-person.getXInc()); }
+        
+        // at right edge, so negate xInc
+        if (person.getXCoord() >= width - DIAMETER) { person.setXInc(-person.getXInc()); }
 
+        // at top edge, so negate yInc
+		if (person.getYCoord() <= 0) { person.setYInc(-person.getYInc()); }
+        
+        // at bottom edge, so negate yInc
+		if (person.getYCoord() >= height - DIAMETER) { person.setYInc(-person.getYInc()); }
+        
 		// adjust the person position
 		person.setXCoord(person.getXCoord() + person.getXInc());
 		person.setYCoord(person.getYCoord() + person.getYInc());
@@ -83,15 +125,34 @@ public class Person {
         int deltaX = person1.getXCoord() - person2.getXCoord();
         int deltaY = person1.getYCoord() - person2.getYCoord();
 
+        // if collision occurs
         if (Math.sqrt(deltaX * deltaX + deltaY * deltaY) <= DIAMETER) {
-            // TODO:
-            // 1. Change the direction of the movement after the collision.
-            // 2. Determine what state variables should be changed and change them.
+            // determine if the person is alive
+            if (person1.isAlive()) {
+                // if the person is not infected yet, then determine if the person will get infected
+                if (!person1.isInfected() && person2.isInfected()) {
+                    Random rand = new Random();
+                    person1.setIsInfected(rand.nextDouble(1.0) <= person1.getINFECT_CHANCE(person1.getImmunityStatus()));
+
+                    if (person1.isInfected()) { person1.setColour(Color.RED); }
+                }
+                person1.setXInc((int)(Math.random() * 10 - 5));
+                person1.setYInc((int)(Math.random() * 10 - 5));
+            }
+            
+            // same for the second person
+            if (person2.isAlive()) {
+                if (!person2.isInfected() && person1.isInfected()) {
+                    Random rand = new Random();
+                    person2.setIsInfected(rand.nextDouble(1.0) < person2.getINFECT_CHANCE(person2.getImmunityStatus()));
+
+                    if (person2.isInfected()) { person2.setColour(Color.RED); }
+                }
+                person2.setXInc((int)(Math.random() * 10 - 5));
+                person2.setYInc((int)(Math.random() * 10 - 5));
+            }
         }
     } // end checkCollision()
-
-
-
 
 
 
@@ -183,6 +244,14 @@ public class Person {
 
     public static int getDiameter() {
         return DIAMETER;
+    }
+
+    public double getINFECT_CHANCE(int index) {
+        return this.INFECT_CHANCE[index];
+    }
+
+    public double getDEATH_CHANCE(int index) {
+        return this.DEATH_CHANCE[index];
     }
 
 } // end class
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 import javax.swing.*;
 
 /**
@@ -15,7 +16,7 @@ public class Simulation extends JPanel {
     // TODO:
     // 1. Make POPULATION a user-defined value.
     // 2. Add other user-defined parameters (Levels of immunity for specified percentages of the population).
-    private final int POPULATION = 100;
+    private final int POPULATION = 600;
     private Person[] personArray;
 
     /**
@@ -25,10 +26,15 @@ public class Simulation extends JPanel {
         this.time = new Timer(REFRESH_TIME, new CollisionListener());
 
         personArray = new Person[POPULATION];
+        // the first infected
+        personArray[0] = new Person(0, WIDTH, HEIGHT);
+
         // TODO:
-        // Populate the array with objects (just a dummy for now).
-        for (int i = 0; i < personArray.length; i++) {
-            personArray[i] = new Person();
+        // Populate the array with objects based on the user specified values.
+
+        // array with dummy population, (no one has immunity)
+        for (int i = 1; i < personArray.length; i++) {
+            personArray[i] = new Person(1, WIDTH, HEIGHT);
         }
 
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -63,14 +69,40 @@ public class Simulation extends JPanel {
     private class CollisionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            // move the population around with move()
             for (int i = 0; i < POPULATION; i++) {
+                // move the population around with move()
                 Person.move(personArray[i], WIDTH, HEIGHT);
+
+                // increment the repaint cycle counter if the person is infected
+                if (personArray[i].isInfected()) {
+                    personArray[i].setCycleCount(personArray[i].getCycleCount() + 1);
+
+                    // check if the counter >= 150 and determine the person's state
+                    if (personArray[i].getCycleCount() >= 150) {
+                        Random rand = new Random();
+                        personArray[i].setIsAlive(!(rand.nextDouble(1.0) <= personArray[i].getDEATH_CHANCE(personArray[i].getImmunityStatus())));
+
+                        if (personArray[i].isAlive()) {
+                            personArray[i].setColour(Color.GREEN);
+
+                            int newImmunityStatus = personArray[i].getImmunityStatus() < 3 ? 5 : personArray[i].getImmunityStatus();
+                            personArray[i].setImmunityStatus(newImmunityStatus);
+                        }
+                        else {
+                            personArray[i].setColour(Color.BLACK);
+
+                            personArray[i].setXInc(0);
+                            personArray[i].setYInc(0);
+                        }
+
+                        personArray[i].setIsInfected(false);
+                    }
+                }
             }
 
             // check for collisions among the population
             for (int i = 0; i < POPULATION; i++) {
-                for (int j = 0; j < POPULATION; j++) {
+                for (int j = i + 1; j < POPULATION; j++) {
                     Person.checkCollision(personArray[i], personArray[j]);
                 }
             }
