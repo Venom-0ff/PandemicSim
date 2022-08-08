15e669519ce628c69939732b314dd3a7d2176b30
Venom-0ff <90479045+Venom-0ff@users.noreply.github.com>
2022-08-08 18:05:13 -0400

    Added showcase of the simualtion results.

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,10 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Set;
 import java.util.Random;
-import javax.swing.*;
-import javax.swing.border.Border;
-
 import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.swing.*;
 
 /**
 * The {@code Simulation} class represents a spread of infection among population.
@@ -16,9 +17,12 @@ public class Simulation extends JPanel {
     private Timer time;
     private int simCycleCount = 0;
     private boolean isOnPause = false;
+    private boolean isFinished = false;
     public boolean isOnPause() { return this.isOnPause; }
-    private final int WIDTH = 800, HEIGHT = 600;      // size of JPanel
-	private final int REFRESH_TIME = 200;             // time in milliseconds between re-paints of the screen
+    public boolean isFinished() { return this.isFinished; }
+    private final int WIDTH = 800, HEIGHT = 600;        // size of JPanel
+	private final int REFRESH_TIME = 200;               // time in milliseconds between re-paints of the screen
+    private final int REPAINT_CYCLES = 450;             // total number of repaint cycles aka duration of the simulation
 	
     private int population = 100;
     private int oneShotPercent = 0;
@@ -26,6 +30,9 @@ public class Simulation extends JPanel {
     private int threeShotsPercent = 0;
     private int recoveredPercent = 0;
     private ArrayList<Person> personArray;
+    private Set<Person> allInfected;
+    private Set<Person> allRecovered;
+    private Set<Person> allDied;
 
     private JProgressBar progressBar;
     private JLabel lblInf;
@@ -59,6 +66,9 @@ public class Simulation extends JPanel {
         this.recoveredPercent = recovered;
 
         personArray = new ArrayList<Person>();
+        allInfected = new HashSet<Person>();
+        allRecovered = new HashSet<Person>();
+        allDied = new HashSet<Person>();
         // the first infected person
         personArray.add(new Person(0, WIDTH, HEIGHT));
 
@@ -98,7 +108,7 @@ public class Simulation extends JPanel {
         progressBar.setStringPainted(true);
 
         JPanel infoPanel = new JPanel();
-        infoPanel.setLayout(new GridLayout(8, 2, 10, 10));
+        infoPanel.setLayout(new GridLayout(8, 2, 20, 0));
 
         infoPanel.add(new JLabel("Number of infected persons:"));
         lblInf = new JLabel(String.valueOf(0));
@@ -228,12 +238,15 @@ public class Simulation extends JPanel {
                 }
             }
 
+            // record all the different data about the population
             for (int i = 0; i < personArray.size(); i++) {
                 if (!personArray.get(i).isAlive()) {
                     diedSum++;
+                    allDied.add(personArray.get(i));
                 }
                 else if (personArray.get(i).isInfected()) {
                     infSum++;
+                    allInfected.add(personArray.get(i));
                     if (personArray.get(i).getImmunityStatus() == 1) { nonVacSum++; }
                     if (personArray.get(i).getImmunityStatus() == 2) { oneShotSum++; }
                     if (personArray.get(i).getImmunityStatus() == 3) { twoShotsSum++; }
@@ -241,7 +254,7 @@ public class Simulation extends JPanel {
                     if (personArray.get(i).getImmunityStatus() == 5) { natImmSum++; }
                 }
                 else {
-                    if (personArray.get(i).getImmunityStatus() == 5) { recovSum++; }
+                    if (personArray.get(i).getImmunityStatus() == 5) { recovSum++; allRecovered.add(personArray.get(i)); }
                 }
                 
             }
@@ -255,6 +268,7 @@ public class Simulation extends JPanel {
             lblRecovered.setText(String.valueOf(recovSum));
             lblDied.setText(String.valueOf(diedSum));
 
+
             // check for collisions among the population
             for (int i = 0; i < personArray.size(); i++) {
                 for (int j = i + 1; j < personArray.size(); j++) {
@@ -263,23 +277,87 @@ public class Simulation extends JPanel {
             }
 
             // stop the simulation ofter 450 cycles
-            if (simCycleCount > 450) {
+            if (simCycleCount > REPAINT_CYCLES) {
                 time.stop();
-                // TODO:
-                // After the simulation has finished, use the data generated to calculate and display this information:
-                // 1)	Percentage of the total population that contracted the disease.
-                // 2)	Percentage of unvaccinated persons who contracted the disease.
-                // 3)	Percentage of one-shot-vaccinated persons who contracted the disease.
-                // 4)	Percentage of two-shot-vaccinated persons who contracted the disease.
-                // 5)	Percentage of three-shot-vaccinated persons who contracted the disease.
-                // 6)	Percentage of those naturally immune persons who got re-infected.
-                // 7)	Percentage of all those who contracted the disease that recovered.
-                // 8)	Death Rate Percentage of all those who contracted the disease that died, broken down by their immunity status.
+                isFinished = true;
 
+                // After the simulation has finished, use the data generated to calculate and display this information:
+                double totPerc = allInfected.size() * 100.0 / population;       // 1)	Percentage of the total population that contracted the disease.
+                infSum = 0;
+                nonVacSum = 0;
+                oneShotSum = 0;
+                twoShotsSum = 0;
+                threeShotsSum = 0;
+                natImmSum = 0;
+                recovSum = 0;
+                diedSum = 0;
+                Person[] array = new Person[allInfected.size()];
+                allInfected.toArray(array);
+                for (int i = 0; i < allInfected.size(); i++) {
+                    if (array[i].getImmunityStatus() == 1) { nonVacSum++; }
+                    if (array[i].getImmunityStatus() == 2) { oneShotSum++; }
+                    if (array[i].getImmunityStatus() == 3) { twoShotsSum++; }
+                    if (array[i].getImmunityStatus() == 4) { threeShotsSum++; }
+                    if (array[i].getImmunityStatus() == 5) { natImmSum++; }
+                }
+                double unvacPerc = nonVacSum * 100.0 / (population - ((population * oneShotPercent / 100)
+                                                                    + (population * twoShotsPercent / 100)
+                                                                    + (population * threeShotsPercent / 100)
+                                                                    + (population * recoveredPercent / 100)));          // 2)	Percentage of unvaccinated persons who contracted the disease.
+                double oneShotPerc = oneShotSum * 100.0 / (population * oneShotPercent / 100);                          // 3)	Percentage of one-shot-vaccinated persons who contracted the disease.
+                double twoShotPerc = twoShotsSum * 100.0 / (population * twoShotsPercent / 100);                        // 4)	Percentage of two-shot-vaccinated persons who contracted the disease.
+                double threeShotPerc = threeShotsSum * 100.0 / (population * threeShotsPercent / 100);                  // 5)	Percentage of three-shot-vaccinated persons who contracted the disease.
+                double natImmPerc = natImmSum * 100.0 / (population * recoveredPercent / 100);                          // 6)	Percentage of those naturally immune persons who got re-infected.
+                double recPerc = allRecovered.size() * 100.0 / population;                                              // 7)	Percentage of all those who contracted the disease that recovered.
+                                                                                                                        // 8)	Death Rate Percentage of all those who contracted the disease that died, broken down by their immunity status.
+                int deadUnvac = 0;
+                int dead1 = 0;
+                int dead2 = 0;
+                int dead3 = 0;
+                int deadNat = 0;
+                Person[] dead = new Person[allDied.size()];
+                allDied.toArray(dead);
+                for (int i = 0; i < allDied.size(); i++) {
+                    if (dead[i].getImmunityStatus() == 1) { deadUnvac++; }
+                    if (dead[i].getImmunityStatus() == 2) { dead1++; }
+                    if (dead[i].getImmunityStatus() == 3) { dead2++; }
+                    if (dead[i].getImmunityStatus() == 4) { dead3++; }
+                    if (dead[i].getImmunityStatus() == 5) { deadNat++; }
+                }
+                double deadUnvacPerc = deadUnvac * 100.0 / nonVacSum;
+                double dead1Perc = dead1 * 100.0 / oneShotSum;
+                double dead2Perc = dead2 * 100.0 / twoShotsSum;
+                double dead3Perc = dead3 * 100.0 / threeShotsSum;
+                double deadNatPerc = deadNat * 100.0 / natImmSum;
+
+                String[][] data = {
+                    {"Total Infected", String.valueOf(totPerc) + "%"},
+                    {"Unvaccinated Infected", String.valueOf(unvacPerc) + "%"},
+                    {"One-Shot Infected", String.valueOf(oneShotPerc) + "%"},
+                    {"Two-Shots Infected", String.valueOf(twoShotPerc) + "%"},
+                    {"Three-Shots Infected", String.valueOf(threeShotPerc) + "%"},
+                    {"Re-Infected", String.valueOf(natImmPerc) + "%"},
+                    {"Total Recovered", String.valueOf(recPerc) + "%"},
+                    {"Unvaccinated Died", String.valueOf(deadUnvacPerc) + "%"},
+                    {"One-Shot Died", String.valueOf(dead1Perc) + "%"},
+                    {"Two-Shots Died", String.valueOf(dead2Perc) + "%"},
+                    {"Three-Shots Died", String.valueOf(dead3Perc) + "%"},
+                    {"Naturally Immune Died", String.valueOf(deadNatPerc) + "%"}
+                };
+                String[] col = {"Measure","Value %"};
+                JTable results = new JTable(data, col);
+                JScrollPane sp = new JScrollPane(results);
+                JFrame resultFrame = new JFrame();
+                resultFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                resultFrame.add(sp);
+                resultFrame.setLocationRelativeTo(null);
+                resultFrame.setSize(800, 600);
+                resultFrame.setVisible(true);
+                
             }
             else {
                 // fill the progressBar
-                progressBar.setValue(simCycleCount * 100 / 450);
+                progressBar.setValue(simCycleCount * 100 / REPAINT_CYCLES);
             }
             
             simCycleCount++;
